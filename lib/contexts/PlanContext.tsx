92b9c4a5096d61983630ff934ebd1d39429953aa
Joshua Gato <joshuagato37@gmail.com>
2025-12-09 17:53:59 +0000

    Reformatted file

--- lib/contexts/PlanContext.tsx
+++ lib/contexts/PlanContext.tsx
@@ -3,42 +3,42 @@
 import { createContext, useContext } from "react";
 
 interface PlanContextType {
-  isFreeUser: boolean;
-  hasProPlan: boolean;
-  hasEnterprisePlan: boolean;
+    isFreeUser: boolean;
+    hasProPlan: boolean;
+    hasEnterprisePlan: boolean;
 }
 
 const PlanContext = createContext<PlanContextType | undefined>(undefined);
 
 interface PlanProviderProps {
-  children: React.ReactNode;
-  hasProPlan: boolean;
-  hasEnterprisePlan: boolean;
+    children: React.ReactNode;
+    hasProPlan: boolean;
+    hasEnterprisePlan: boolean;
 }
 
 export function PlanProvider({
-  children,
-  hasProPlan,
-  hasEnterprisePlan,
+    children,
+    hasProPlan,
+    hasEnterprisePlan,
 }: PlanProviderProps) {
-  return (
-    <PlanContext.Provider
-      value={{
-        hasProPlan,
-        hasEnterprisePlan,
-        isFreeUser: !hasProPlan && !hasEnterprisePlan,
-      }}
-    >
-      {children}
-    </PlanContext.Provider>
-  );
+    return (
+        <PlanContext.Provider
+            value={{
+                hasProPlan,
+                hasEnterprisePlan,
+                isFreeUser: !hasProPlan && !hasEnterprisePlan,
+            }}
+        >
+            {children}
+        </PlanContext.Provider>
+    );
 }
 
 export const usePlan = () => {
-  const context = useContext(PlanContext);
-  if (context === undefined) {
-    throw new Error("usePlan needs to be inside the provider");
-  }
+    const context = useContext(PlanContext);
+    if (context === undefined) {
+        throw new Error("usePlan needs to be inside the provider");
+    }
 
-  return context;
+    return context;
 };
